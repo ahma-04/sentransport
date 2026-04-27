3c8e5f51778d538d79e440c575d51a4482f90f0c
Serigne Ahma <ahma12mar34@gmail.com>
2026-04-27 17:06:34 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique3.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique3() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">25</span>
+      <span className="stat-libelle">bus</span>
+    </div>
+  );
+}
+
+export default Statistique3;
